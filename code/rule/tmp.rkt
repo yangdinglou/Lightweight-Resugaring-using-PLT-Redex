daf0804f92093e32686c159976b0c1b510538dc4
yangdinglou <1079179461@qq.com>
2020-06-22 03:33:09 +0800

    functions of automata nearly finished, with bugs in alias merging

--- code/rule/tmp.rkt
+++ code/rule/tmp.rkt
@@ -1,37 +1,41 @@
 #lang racket
 (require redex)
+(require racket/hash)
 (struct IFA ([Q #:mutable]
              [Σ #:mutable]
              [δ #:mutable]
              [s #:mutable]
              [F #:mutable]
-             [subst #:mutable])
+             [subst #:mutable]
+             [alias #:mutable])
   #:transparent)
 
 (define (get-tmp-IFA head)
-  (case head
-    [('let) IFA-LET]
-    [('if) IFA-IF]))
+  (cond 
+    [(equal? head 'let) IFA-LET]
+    [(equal? head 'if) IFA-IF]))
 (define IFA-IF 
   (IFA (list (term oe_1) (term oe_2) (term oe_3))
        (list (term (if oe_1 oe_2 oe_3)) (term (if #t oe_2 oe_3)) (term (if #f oe_2 oe_3)))
        (hash (term oe_1) (hash 'void (term oe_1) #t (term oe_2) #f (term oe_3)))
        (list (term oe_1))
        (list (term oe_2) (term oe_3))
-       (list)
+       (hash)
+       (hash)
        ))
 ;(hash-ref (hash-ref (IFA-δ IFA-IF) 'oe_1) 'void)
 
 ;(let x e1 e2)-->(let x e1' e2)
 ;(let x v1 e2)-->(subst x v1 e2)
 (define IFA-LET 
-  (IFA (list (term x) (term oe_1) (term ov_1)
-             (term (subst x ov_1 oe_2)))
-       (list (term (let x oe_1 oe_2)) (term (let x ov_1 oe_2)))
-       (hash (term oe_1) (hash 'void (term oe_1) (term ov_1) (term (ov_1))) (term ov_1) (hash 'ov_1 '(subst x ov_1 oe_2)))
+  (IFA (list (term ox) (term oe_1) (term ov_1)
+             (term oe_2))
+       (list (term (let ox oe_1 oe_2)) (term (leto x ov_1 oe_2)))
+       (hash (term oe_1) (hash 'void (term oe_1) (term ov_1) (term ov_1)) (term ov_1) (hash 'ov_1 'oe_2))
        (list (term oe_1))
-       (list '(subst x ov_1 oe_2))
-       (list)
+       (list 'oe_2)
+       (hash 'oe_2 (hash 'ox 'ov_1) 'ox (hash 'ox 'ox))
+       (hash)
        ))
 
 (define IFA-TMP
@@ -40,7 +44,8 @@
        (hash (term oe_1) (hash '#t (term oe_2) '#f '#f) (term oe_2) (hash '#t '#f '#f '#t))
        (list (term oe_1))
        (list '#t '#f)
-       (list)
+       (hash)
+       (hash)
        ))
 ;(or e1 e2)-->(let x e1 (if x x e2))
 ;(map f e)-->(if (null? e) (list) (cons (f (first e)) (map f (rest e))))
@@ -57,6 +62,7 @@
       (if (equal? trm old_term) new_term trm)))
 
 (define (modify-hash hash-table old_term new_term)
+  
   (let ((tmp-key (hash-keys hash-table)) (tmp-hash (make-hash)))
     (begin
       (for ((key tmp-key))
@@ -75,32 +81,132 @@
 #;(modify-term (list (term x) (term oe_1) 
              (term (subst x ov_1 oe_2))) 'ov_1 'v_1)
 (define (modify-IFA old-IFA loc new_term)
-  (let ([tmpQ (IFA-Q old-IFA)] [tmpΣ (IFA-Σ old-IFA)] (tmpδ (IFA-δ old-IFA)) (tmps (IFA-s old-IFA)) (tmpF (IFA-F old-IFA)) (tmpsubst (IFA-subst old-IFA)))
-    (let ((old_term (list-ref (car tmpΣ) loc)))
+  (let ((tmpQ (IFA-Q old-IFA)) (tmpΣ (IFA-Σ old-IFA)) (tmpδ (IFA-δ old-IFA))
+                               (tmps (IFA-s old-IFA)) (tmpF (IFA-F old-IFA))
+                               (tmpsubst (IFA-subst old-IFA)) (tmpalias (IFA-alias old-IFA)))
+    (let ((old_term (list-ref (car tmpΣ) loc)) (tmp_term new_term))
       (begin (set! tmpQ (modify-term tmpQ old_term new_term))
+             (if (check-duplicates tmpQ)
+                 (let ((tmplength (if (hash-has-key? tmpalias tmp_term) (length (hash-ref tmpalias new_term)) 0) ))
+                   (begin
+                     (set! new_term (string->symbol (string-append (symbol->string new_term) "_a" (number->string tmplength))))
+                     (set! tmpQ (list-set tmpQ (last (indexes-of tmpQ tmp_term)) new_term))
+                     (hash-set! tmpalias tmp_term (if (hash-has-key? tmpalias tmp_term)
+                                                                     (append (hash-ref tmpalias tmp_term) (list new_term))
+                                                                     (list new_term)))))
+                 (void))
              (set! tmpΣ (modify-term tmpΣ old_term new_term))
              (set! tmpδ (modify-hash tmpδ old_term new_term))
              (set! tmps (modify-term tmps old_term new_term))
              (set! tmpF (modify-term tmpF old_term new_term))
-             (set! tmpsubst (modify-term tmpsubst old_term new_term))
-             (IFA tmpQ tmpΣ tmpδ tmps tmpF tmpsubst))));todo:add node merge
+             (set! tmpsubst (modify-hash tmpsubst old_term new_term))
+             (set! tmpalias (modify-hash tmpalias old_term new_term))
+             (IFA tmpQ tmpΣ tmpδ tmps tmpF tmpsubst tmpalias))))
   )
+;(modify-IFA IFA-IF 1 'x)
 ;(modify-IFA (modify-IFA (modify-IFA IFA-IF 1 'x) 2 'x) 3 'e_2)
-(define (merge-IFA old-IFA loc new_term)
-  (let ([tmpQ (IFA-Q old-IFA)] [tmpΣ (IFA-Σ old-IFA)] (tmpδ (IFA-δ old-IFA)) (tmps (IFA-s old-IFA)) (tmpF (IFA-F old-IFA)) (tmpsubst (IFA-subst old-IFA)))
-    (let ((old_term (list-ref (car tmpΣ) loc)) (tmpIFA (build-IFA '(1 (list) new_term))))
-      (begin
-        (void);todo
-        )))
+
+(define build-IFA
+  (λ (rule)
+    (case (car rule)
+      [(1) (let ((tmp (get-tmp-IFA (car (list-ref rule 2))))) ;
+             (begin
+               (for ((i (- (length (list-ref rule 2)) 1)))
+                 (if (pair? (list-ref (list-ref rule 2) (+ i 1)))
+                     (set! tmp (merge-IFA tmp (+ i 1) (list-ref (list-ref rule 2) (+ i 1))))
+                     (set! tmp (modify-IFA tmp (+ i 1) (list-ref (list-ref rule 2) (+ i 1))))))
+               tmp))])))
+(define (merge-hash old_term old-IFA subIFA)
+  (if (member old_term (IFA-F old-IFA))
+      (let ((tmphash (modify-hash (IFA-δ old-IFA) old_term (car (IFA-s subIFA)))))
+        (begin (hash-union! tmphash (IFA-δ subIFA))tmphash))
+      
+      (let ((tmphash (IFA-δ old-IFA)) (old_trans (hash-ref (IFA-δ old-IFA) old_term)))
+        (begin
+          (for ((tmpkey (IFA-F subIFA)))
+            (hash-set! tmphash tmpkey old_trans))
+          (hash-remove! tmphash old_term)
+          (set! tmphash (modify-hash tmphash old_term (car (IFA-s subIFA))))
+          (hash-union! tmphash (IFA-δ subIFA))
+          tmphash)))
   )
+(define (merge-IFA old-IFA loc new_term)
+  (let ([tmpQ (IFA-Q old-IFA)]
+        [tmpΣ (IFA-Σ old-IFA)] (tmpδ (IFA-δ old-IFA))
+        (tmps (IFA-s old-IFA)) (tmpF (IFA-F old-IFA))
+        (tmpsubst (hash-copy (IFA-subst old-IFA))) (tmpalias (hash-copy (IFA-alias old-IFA))))
+    (let ((old_term (list-ref (car tmpΣ) loc)) (subIFA (build-IFA (list 1 (list) new_term))))
+      (begin
+        (set! tmpQ (append tmpQ (IFA-Q subIFA)))
+        (if (check-duplicates tmpQ)
+            (let ((dup_term (check-duplicates tmpQ))
+                  (tmplength (if (hash-has-key? tmpalias (check-duplicates tmpQ)) (length (hash-ref tmpalias (check-duplicates tmpQ))) 0)))
+              (begin
+                (if (hash-has-key? (IFA-alias subIFA) dup_term)
+                    (begin
+                      (if (eq? tmplength 0)
+                          (hash-set! tmpalias dup_term (hash-ref (IFA-alias subIFA) dup_term))
+                          (for ((ali-term (hash-ref (IFA-alias subIFA) dup_term)))
+                            (let ((tmpnew_term (string->symbol (string-append (symbol->string dup_term) "_a" (number->string tmplength)))))
+                              (begin
+                                (set! subIFA
+                                      (modify-IFA subIFA
+                                                  (index-of (car tmpΣ) ali-term)
+                                                  tmpnew_term);todo
+                                      )
+                                (hash-set! tmpalias dup_term (append (hash-ref tmpalias dup_term) (list tmpnew_term)))
+                                ))))
+                      
+                      (let ((copyalias (hash-copy (IFA-alias subIFA))))
+                        ;(set-IFA-alias! subIFA (hash-remove copyalias dup_term))
+                        (begin
+                          (hash-remove! copyalias dup_term)
+                          (set-IFA-alias! subIFA copyalias))
+                        )
+                      ;(hash-remove! subIFA dup_term)
+                      )
+                    (void))
+                (let ((tmpnew_term (string->symbol (string-append (symbol->string dup_term) "_a" (number->string tmplength)))))
+                  (begin
+                    (set! subIFA
+                          (modify-IFA subIFA
+                                      (index-of (car tmpΣ) dup_term)
+                                      tmpnew_term)
+                          )
+                    (hash-set! tmpalias dup_term (append (hash-ref tmpalias dup_term) (list tmpnew_term)))
+                    ))
+                ))
+            (void))
+        (displayln tmpδ)
+        (displayln old_term)
+        (displayln old-IFA)
+        (displayln subIFA)
+        (set! tmpδ (merge-hash old_term old-IFA subIFA))
+        (if (member old_term tmps)
+            (set! tmps (IFA-s subIFA))
+            (void))
+        (if (member old_term tmpF)
+            (set! tmpF (append (remove old_term tmpF) (IFA-F subIFA)))
+            (void))
+        (if (hash-has-key? tmpsubst old_term)
+            (for ((tmpkey (IFA-Q subIFA)))
+              (begin
+                (hash-set! tmpsubst tmpkey (hash-ref tmpsubst old_term))
+                (if (hash-has-key? (IFA-subst subIFA) tmpkey)
+                    (let ((innersubst (hash-ref (IFA-subst subIFA) tmpkey))
+                          (outersubst (hash-ref tmpsubst tmpkey)))
+                      (begin
+                        (for ((tmpkey2 (hash-keys innersubst)))
+                          (hash-set! outersubst tmpkey2 (hash-ref innersubst tmpkey2)))
+                        (hash-set! tmpsubst tmpkey outersubst)))
+                    (void))))
+                    
+            (hash-union! tmpsubst (IFA-subst subIFA)))
+        (hash-union! tmpalias (IFA-alias subIFA))
+        (IFA tmpQ tmpΣ tmpδ tmps tmpF tmpsubst tmpalias)
+        )
+      ))
+  )
+
+(merge-IFA (modify-IFA (modify-IFA (modify-IFA IFA-LET 1 'x) 2 'e_1) 3 'e_2) 3 '(if x x e_2))
   
-  
-(define (build-IFA rule)
-  (case (car rule)
-    [(1) (let ((tmp (get-tmp-IFA (car (list-ref rule 2)))))
-           (begin
-             (for ((i (- (length (list-ref rule 2)) 1)))
-               (if (pair? (list-ref (list-ref rule 2) (+ i 1)))
-                   (set! tmp (merge-IFA tmp (+ i 1) (list-ref (list-ref rule 2) (+ i 1))))
-                   (set! tmp (modify-IFA tmp (+ i 1) (list-ref (list-ref rule 2) (+ i 1))))))
-             tmp))]))
